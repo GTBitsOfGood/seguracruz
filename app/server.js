e8b96a6c5494413fdb22591b1823817dee304ac7
Michael Koohang <michael.s.k1001@gmail.com>
2020-12-03 22:54:41 -0500

    Change port back to 8080

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on: ${PORT}`);
