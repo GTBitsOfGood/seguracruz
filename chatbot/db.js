e29db2d33ebaa29c6aa39c8bb2099c9eb55ed761
Kavin Phan <kavin@kphan.tech>
2022-05-11 22:11:40 -0400

    Log database connection status

--- chatbot/db.js
+++ chatbot/db.js
@@ -1,4 +1,5 @@
 const db = require('mariadb');
+const winston = require('./res/winston');
 
 const pool = db.createPool({
   host: process.env.DB_HOST,
@@ -6,6 +7,14 @@ const pool = db.createPool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   connectionLimit: 5
+});
+
+pool.getConnection().then(p => {
+  if (p.isValid()) {
+    winston.info("connected to database");
+  } else {
+    throw "could not connect to database";
+  }
 });
 
 module.exports = pool;
